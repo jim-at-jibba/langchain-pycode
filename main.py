93dc464965b3991b32b5845a92e62eb22b720a8c
James Best <jim@justjibba.net>
2024-02-02 10:42:04 +0000

    Added sequential chains

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 
 from dotenv import load_dotenv
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 
@@ -20,11 +20,30 @@ code_prompt = PromptTemplate(
     input_variables=["language", "task"],
 )
 
+test_prompt = PromptTemplate(
+    template="Write a test for the following {language} code:\n\n{code}",
+    input_variables=["language", "code"],
+)
+
 code_chain = LLMChain(
     llm=llm,
     prompt=code_prompt,
+    output_key="code",
+)
+
+test_chain = LLMChain(
+    llm=llm,
+    prompt=test_prompt,
+    output_key="test",
 )
 
 
-result = code_chain({"language": args.language, "task": args.task})
-print(result["text"])
+chain = SequentialChain(
+    chains=[code_chain, test_chain],
+    input_variables=["language", "task"],
+    output_variables=["code", "test"],
+)
+
+result = chain({"language": args.language, "task": args.task})
+print(result["code"])
+print(result["test"])
